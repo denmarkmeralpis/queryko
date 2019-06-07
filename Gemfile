837b91c469aecec0316735e49da85ad8dbaaa69b
Nujian Den Mark Meralpis <denmarkmeralpis@gmail.com>
2019-06-07 13:50:29 +0800

    Use MySQL as db adpater

--- Gemfile
+++ Gemfile
@@ -4,3 +4,7 @@ git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
 # Specify your gem's dependencies in queryko.gemspec
 gemspec
+
+group :test do
+   gem 'mysql2'
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,8 +19,53 @@ RSpec.configure do |config|
   end
 end
 
-ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: ':memory:')
+# ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: ':memory:')
 migration_files_path = File.expand_path('../fixtures/app/migrations', __FILE__)
+
+module Db
+  # db is MySQL
+  module MySQL
+    def connect_db
+      ActiveRecord::Base.establish_connection(db_config)
+    end
+
+    def drop_and_create_database
+      temp_connection = db_config.merge(database: 'mysql')
+
+      ActiveRecord::Base.establish_connection(temp_connection)
+      # drop existing db if exists
+      ActiveRecord::Base.connection.drop_database(db_name) rescue nil
+      # create new db
+      ActiveRecord::Base.connection.create_database(db_name)
+    end
+
+    def db_config
+      @db_config ||= {
+        adapter:   'mysql2',
+        database:  db_name,
+        username:  db_user,
+        password: db_password
+      }
+    end
+
+    def db_user
+      ENV['TRAVIS_DB_USER'] ? 'travis' : 'root'
+    end
+
+    def db_password
+      ENV['TRAVIS_DB_PASSWORD'] ? '' : 'root'
+    end
+
+    def db_name
+      'dm_filter_test'
+    end
+  end
+end
+
+include Db::MySQL
+
+drop_and_create_database
+connect_db
 
 if ActiveRecord.gem_version >= Gem::Version.new('5.2.2')
   ActiveRecord::MigrationContext.new(migration_files_path).migrate
